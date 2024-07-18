a2ff03b49d3e205be04cee0305cebc3312a5232b
diazhdez <jd5531066@gmail.com>
2024-07-17 19:15:50 -0600

    ya

--- database/database.py
+++ database/database.py
@@ -1,25 +1,18 @@
 import mysql.connector
 import os
 
-# Obtener las variables de entorno
-MYSQL_HOST = os.environ.get('MYSQL_HOST')
-MYSQL_USER = os.environ.get('MYSQL_USER')
-MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
-MYSQL_DATABASE = os.environ.get('MYSQL_DATABASE')
 
 def dbConnection():
     try:
         # Crear la conexión a la base de datos MySQL
         connection = mysql.connector.connect(
-            host=MYSQL_HOST,
-            user=MYSQL_USER,
-            password=MYSQL_PASSWORD,
-            database=MYSQL_DATABASE
+            host='sql5.freemysqlhosting.net',
+            user='sql5720463',
+            password='EUTLx1IP9A',
+            database='sql5720463'
         )
         print("Conexión exitosa a la base de datos MySQL")
         return connection
     except mysql.connector.Error as err:
         print("Error de conexión a la base de datos MySQL:", err)
         return None
-
-dbConnection()
